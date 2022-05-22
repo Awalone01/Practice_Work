59d533210a2a15783d34611fb55f8053be614cb6
finalWork <sashaneiv01@yandex.ru>
2022-05-22 21:07:51 +0300

    Final commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String alphabet = "a b c d";
+        String user = "Alex|qwerty@mail.ru";
+
+        String[] splitUser = user.split("\\|");
+        String[] splitAlphabet = alphabet.split(" ", 2);
+
+        for (String word : splitAlphabet) {
+            System.out.println(word);
+        }
+
+        for (String word : splitUser) {
+            System.out.println(word);
+        }
     }
 }
